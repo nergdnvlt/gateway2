7e504a633417b48ffddd4c7418b86c75d8ede87a
aleonardo <adrienne@apollographql.com>
2022-01-12 13:18:14 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+//needed to load env vars to be able to connect to Apollo Studio
+require('dotenv').config();
+
+const port = process.env.PORT // || 4000;
+
+
+const gateway = new ApolloGateway();
+
+const server = new ApolloServer({
+  gateway
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Gateway ready at ${url}`);
+}).catch(err => {console.error(err)});
